3f5645abe81919d3b419cedaa78902b940a99c30
Serzhbelik <sergeybelik86@gmail.com>
2020-02-10 10:39:50 +0300

    done!

--- app/src/main/java/com/example/helloapplication/MainActivity.kt
+++ app/src/main/java/com/example/helloapplication/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.helloapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = text
+        textView.text = """
+            Buildtype = ${BuildConfig.BUILD_TYPE}
+            Flavor = ${BuildConfig.FLAVOR}
+            Version code = ${BuildConfig.VERSION_CODE}
+            Version name = ${BuildConfig.VERSION_NAME}
+            Application ID = ${BuildConfig.APPLICATION_ID}
+        """.trimIndent()
     }
 }
